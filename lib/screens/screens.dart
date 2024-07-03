f9d1477f3bd59ad4464409a142ae47dd9a945989
brayan <br4y47.r4m0s@gmail.com>
2024-07-03 10:56:21 -0400

    Comienzo de la app productos Login

---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+
+export 'package:productos/screens/home_screen.dart';
+export 'package:productos/screens/login_screen.dart';
+
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,4 @@
+
+
+export 'package:productos/widgets/auth_background.dart';
+
